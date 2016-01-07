1b31fc604ab2f77a2c23b7f5dbda541ebd65eb3c
eharbers <erik.harbers64@gmail.com>
2016-01-07 22:10:33 +0100

    p61

--- cars.py
+++ cars.py
@@ -2,3 +2,15 @@ cars= ['bmw','audi','toyota','subaru']
 print(cars)
 cars.sort()
 print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+print("Here is the origina list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
