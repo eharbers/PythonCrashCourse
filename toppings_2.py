6349836170e48bdd298b311edc8b7c62eda80bcb
eharbers <erik.harbers64@gmail.com>
2016-01-11 19:28:37 +0100

    p91

---
+++ toppings_2.py
@@ -0,0 +1,11 @@
+requested_toppings = ['mushrooms','green peppers','extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of " + requested_topping + ".")
+    else:
+        print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+
+
