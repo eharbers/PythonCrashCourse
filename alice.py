69060f3beb6913f861071fcf8bb74dac545a166e
eharbers <erik.harbers64@gmail.com>
2016-01-17 12:22:30 +0100

    aanp pi birthday

---
+++ alice.py
@@ -0,0 +1,5 @@
+filename = 'alice.txt'
+
+with open(filename) as f_obj:
+    contents = f_obj.read()
+
--- pi_birthday_string.py
+++ pi_birthday_string.py
@@ -13,19 +13,18 @@ print(len(pi_string))
 birthday = raw_input("Enter your birhtday, in the form mmddyy: ")
 if birthday in pi_string:
     print("Your birthday appears in the first million digits of pi!")
+    index = pi_string.index(birthday)
+    print ("Starting at position: " + str(index))
+#    print (pi_string[index:index+6])
 else:
     print("Your birthday does not appear in the first million digits of pi.")
 
-index = pi_string.index(birthday)
-print (index)
-print (pi_string[index:index+6])
+#teller = 1
 
-teller = 1
+#print(teller, len(pi_string))
 
-print(teller, len(pi_string))
-
-while birthday in pi_string[teller:]:
-    teller = pi_string[teller:].index(birthday)
-    print(teller)
-    if teller == 0:
-        break
+#while birthday in pi_string[teller:]:
+#    teller = pi_string[teller:].index(birthday)
+#    print(teller)
+#    if teller == 0:
+#        break
