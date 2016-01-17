883399d1bffeb600d1c79ef6afb74c651bc7dee5
eharbers <erik.harbers64@gmail.com>
2016-01-17 23:05:08 +0100

    p214

--- remember_me.py
+++ remember_me.py
@@ -1,16 +1,32 @@
 import json
 
-# Load the usernam, if it has been stored previously.
-# Otherwise, prompt for the username and store it.
-filename = 'username.json'
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-except IOError:
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except IOError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
     username = raw_input("What is your name? ")
+    filename = 'username.json'
     with open(filename, 'w') as f_obj:
         json.dump(username, f_obj)
-        print("We'll remember you when you come back, " + username + "!")
-else:
-    print("Welcome back, " + username + "!")
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+      username = get_new_username()
+      print("We'll remember you when you come back, " + username + "!")
+
+greet_user()
 
