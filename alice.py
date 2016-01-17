71fde2cf593e35e60f5e24e76b635a653a572e3c
eharbers <erik.harbers64@gmail.com>
2016-01-17 20:05:51 +0100

    p208

--- alice.py
+++ alice.py
@@ -1,5 +1,16 @@
 filename = 'alice.txt'
 
-with open(filename) as f_obj:
-    contents = f_obj.read()
+try:
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+
+except IOError:
+    msg = "Sorry, the file " + filename + " does not exist."
+    print(msg)
+
+else:
+    # COunt the approximate number of words in the file.
+    words = contents.split()
+    num_words = len(words)
+    print("The file " + filename + " has about " + str(num_words) + " words.")
 
